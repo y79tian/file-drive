0146232bc9e20d5418fc7a10c7276f38d07ff980
y79tian <y79tian@uwaterloo.ca>
2024-06-18 22:20:03 -0400

    adding the upload file dialog

--- convex/files.ts
+++ convex/files.ts
@@ -2,6 +2,15 @@ import { MutationCtx, QueryCtx, mutation, query } from "./_generated/server";
 import { ConvexError, v } from "convex/values";
 import { getUser } from "./users";
 
+export const generateUploadUrl = mutation(async (ctx) => {
+  const identity = await ctx.auth.getUserIdentity();
+  if (!identity) {
+    throw new ConvexError("you must be logged in to upload a file");
+  }
+  return await ctx.storage.generateUploadUrl();
+});
+
+
 async function hasAccessToOrg(
   ctx: MutationCtx | QueryCtx,
   tokenIdentifier: string,
@@ -16,6 +25,7 @@ async function hasAccessToOrg(
 export const createFile = mutation({
   args: {
     name: v.string(),
+    fileId: v.id("_storage"),
     orgId: v.string(),
   },
   async handler(ctx, args) {
@@ -33,6 +43,7 @@ export const createFile = mutation({
     }
     await ctx.db.insert("files", {
       name: args.name,
+      fileId: args.fileId,
       orgId: args.orgId,
     });
   },
